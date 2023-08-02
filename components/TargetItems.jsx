03a6102ea024baf8ddd13ff5fedac8a96a71fa4a
eliasmartins <rhyanlucaseliasmartins@gmail.com>
2023-08-02 13:27:10 -0300

    color target

--- components/TargetItems.jsx
+++ components/TargetItems.jsx
@@ -21,12 +21,10 @@ export default function TargetItems({ muscle }) {
   const removeFavorite = () => dispatch(setRemoveFromFavorites(muscle));
 
   return (
-    <div className="lg:grid grid-cols-1 border-b-8 border-yellow-500 mb-10">
+    <div className="lg:grid grid-cols-1 mb-10 border-b border-spacing-y-80">
       <div className="">
         <div className="flex-between">
-          <h3 className="text-xl mb-3 font-bold text-yellow-500 border-b capitalize">
-            {exercise_name}
-          </h3>
+          <h3 className="text-xl mb-3 font-bold capitalize">{exercise_name}</h3>
           <button onClick={removeFavorite} className="text-4xl">
             <CiCircleRemove />
           </button>
@@ -54,26 +52,19 @@ export default function TargetItems({ muscle }) {
         </div>
       </div>
       <div className="mt-5">
-        <h4 className="text-xl mb-3 font-bold text-yellow-500 border-b capitalize">
-          Steps
-        </h4>
-        <div className="flex flex-col  gap-y-3">
+        <h4 className="text-xl mb-3 font-bold capitalize">Steps</h4>
+        <div className="flex flex-col gap-y-3">
           {steps.map((step, idx) => (
-            <p key={idx} className="p-4 bg-zinc-200 rounded-xl">
-              <span className="p-2 bg-zinc-50 rounded-full text-yellow-500">
-                {idx + 1}
-              </span>{' '}
-              {` `}
+            <p key={idx} className="p-4 rounded-xl">
+              <span className="p-2 rounded-full">{idx + 1}</span> {` `}
               {step}
             </p>
           ))}
         </div>
       </div>
       <div className="flex flex-col mt-5">
-        <h4 className="text-xl mb-3 font-bold text-yellow-500 border-b capitalize">
-          Targets
-        </h4>
-        <div className="flex gap-x-5">
+        <h4 className="text-xl mb-3 font-bold capitalize">Targets</h4>
+        <div className="flex gap-x-5 mb-5">
           <p>{target?.Primary?.at(0)}</p>
           <p>{target?.Secondary?.at(0)}</p>
           <p>{target?.Tertiary?.at(0)}</p>
